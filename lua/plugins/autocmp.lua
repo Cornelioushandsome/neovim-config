8c4cde347e699b25532e0cda94548f689c6e4408
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-12-23 16:11:58 -0500

    Refactor autocompletion setup in autocmp.lua

--- lua/plugins/autocmp.lua
+++ lua/plugins/autocmp.lua
@@ -1,29 +1,73 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-  },
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-    })
-  end
+	-- Autocompletion
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		-- Snippet Engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				-- {
+				--   'rafamadriz/friendly-snippets',
+				--   config = function()
+				--     require('luasnip.loaders.from_vscode').lazy_load()
+				--   end,
+				-- },
+			},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
+	opts = {
+		keymap = {
+			preset = "default",
+
+			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+		},
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		completion = {
+			-- By default, you may press `<c-space>` to show the documentation.
+			-- Optionally, set `auto_show = true` to show the documentation after a delay.
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
+
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+			},
+		},
+
+		snippets = { preset = "luasnip" },
+
+		fuzzy = { implementation = "lua" },
+
+		-- Shows a signature help window while you type arguments for a function
+		signature = { enabled = true },
+	},
 }
+
 
