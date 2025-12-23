d0bd2e54df28516d33e0f5f3d39b7cd838e203d1
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-12-23 15:25:51 -0500

    Add key mappings for window navigation and Telescope

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
+vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>", { silent = true })
+vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", { silent = true })
+vim.keymap.set("n", "<leader>t", ":split | terminal<CR>", { silent = true })
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+
